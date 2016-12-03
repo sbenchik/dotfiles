49684cbd61b4df5c349e8a73bc15e576c43b66bd
sbenchik <stevebenchik@gmail.com>
2016-12-03 00:55:46 -0500

    Update a lot of files

--- lock.sh
+++ lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $HOME/.files/lock.png ]] && convert /tmp/screen.png $HOME/dotfiles/lock.png -gravity center -composite -matte /tmp/screen.png
+[[ -f $HOME/dotfiles/lock.png ]] && convert /tmp/screen.png $HOME/dotfiles/lock.png -gravity center -composite -matte /tmp/screen.png
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -ui /tmp/screen.png && sleep 2 && xset dpms force standby
+i3lock -ui /tmp/screen.png && sleep 1 && xset dpms force standby
